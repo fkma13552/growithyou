dc6e0667209c6cd692bb4eda6d9b482469aeea02
fkma13552 <ozonplay123@gmail.com>
2021-08-24 00:09:25 +0300

    Update About.js
    link fixed

--- src/About.js
+++ src/About.js
@@ -142,7 +142,7 @@ function About() {
                             </h1>
                             <div className="text-sm-center">
                                 <div className="py-5 buttons">
-                                    <a href="growithyou.com"><button className="btn btn-filled-purple font-weight-bold">ПІДПИСАТИСЯ</button></a>
+                                    <a href="https://www.growithyou.com"><button className="btn btn-filled-purple font-weight-bold">ПІДПИСАТИСЯ</button></a>
                                     <Link to={'/givetoys'}><button className="btn btn-outlined-pink font-weight-bold">ВІДДАТИ ІГРАШКИ</button></Link>
                                 </div>
                             </div>
